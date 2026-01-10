d6ed4588a5b5fe70c72c09414e8b7fa4b7149d90
amir160alg-afk <amir160alg@gmail.com>
2026-01-11 00:06:04 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.google.services) // Применяем плагин Firebase
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = "com.example.tva"
-    compileSdk = 35 // Используем стабильную версию SDK
+    compileSdk = 35 
 
     defaultConfig {
         applicationId = "com.example.tva"
         minSdk = 24
-        targetSdk = 35 // Используем стабильную версию SDK
+        targetSdk = 35 
         versionCode = 1
         versionName = "1.0"
 
